8da3e5dd9d203cd584504503f78118112decb137
Rachel Ruddy <82718260+rachelruddy@users.noreply.github.com>
2026-09-23 14:35:09 -0400

    Add header file for Kalman filter in C

---
+++ kalman_filter_c.h
@@ -0,0 +1,15 @@
+/*
+ * kalman_filter_c.h
+ *
+ *  Created on: Sep 23, 2026
+ *      Author: rachel
+ */
+
+#ifndef INC_KALMAN_FILTER_C_H_
+#define INC_KALMAN_FILTER_C_H_
+
+#include "kalman_filter.h"
+
+void kalman_c(kalman_state* state, float measurement);
+
+#endif /* INC_KALMAN_FILTER_C_H_ */
